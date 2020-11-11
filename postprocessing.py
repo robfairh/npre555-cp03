971dfaafdfbb5beaf25193e9e11eea68ccbd3a77
robfairh <ref3@illinois.edu>
2020-11-11 15:17:58 -0600

    added initial files

---
+++ postprocessing.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def plotcsv_frommoose_temp(file, legend, dire='x'):
+    '''
+    Moltres output is a csv file.
+    This function plots those values.
+    The output is a figure.
+
+    Parameters:
+    -----------
+    file: [string]
+        name of the .csv file
+    save: [string]
+        name of the figure
+    dire: ['x', 'y', 'z']
+        direction of the detector
+    '''
+    file = pd.read_csv(file)
+
+    if dire == 'r':
+        x = np.array(file['x'].tolist())
+        y = np.array(file['y'].tolist())
+        d = np.sqrt(x**2 + y**2)
+    else:
+        d = file[dire].tolist()
+
+    flux = file['flux'].tolist()
+
+    # plt.figure()
+    plt.plot(d, flux, label=legend)
+    plt.xlabel(dire + ' [cm]')
+    plt.ylabel(r'$\phi \left[\frac{n}{cm^2s}\right]$')
+    # plt.savefig(save, dpi=300, bbox_inches="tight")
+    # plt.close()
+
+
+save = 'output'
+file = 'diffusion/input_line_0001.csv'
+legend = 'diffusion'
+plotcsv_frommoose_temp(file, legend, dire='x')
+file = 'p1/input_line_0001.csv'
+legend = 'p1'
+plotcsv_frommoose_temp(file, legend, dire='x')
+plt.legend(loc='upper right')
+plt.savefig(save, dpi=300, bbox_inches="tight")
