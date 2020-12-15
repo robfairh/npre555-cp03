1dc26b3233d32431767758a6beb34ab81472e557
robfairh <ref3@illinois.edu>
2020-12-15 16:23:10 -0300

    added figures

--- postprocessing.py
+++ postprocessing.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def plotcsv_frommoose_temp(file, legend, dire='x'):
+def plotcsv_frommoose_1G(file, save, diff=True, fix=True, dire='x'):
     '''
     Moltres output is a csv file.
     This function plots those values.
@@ -27,34 +27,97 @@ def plotcsv_frommoose_temp(file, legend, dire='x'):
     else:
         d = file[dire].tolist()
 
-    flux = file['flux'].tolist()
+    if diff is True:
+        flux = np.array(file['flux'].tolist())
+        legend = 'Diff'
 
-    # plt.figure()
+    else:
+        flux0 = np.array(file['flux0'].tolist())
+        flux2 = np.array(file['flux2'].tolist())
+        flux = flux0 - 2*flux2
+        legend = 'P3'
+    
+    if fix is False:
+        flux /= max(flux)
+
     plt.plot(d, flux, label=legend)
+    plt.legend(loc='upper right')
     plt.xlabel(dire + ' [cm]')
     plt.ylabel(r'$\phi \left[\frac{n}{cm^2s}\right]$')
-    # plt.savefig(save, dpi=300, bbox_inches="tight")
-    # plt.close()
 
 
-save = 'output'
+def plotcsv_frommoose_multi(file, save, diff=True, fix=True, G=3, dire='x'):
+    '''
+    Moltres output is a csv file.
+    This function plots those values.
+    The output is a figure.
 
-file = 'diffusion/input-1g-fixed_line_0001.csv'
-legend = 'diffusion'
-plotcsv_frommoose_temp(file, legend, dire='x')
+    Parameters:
+    -----------
+    file: [string]
+        name of the .csv file
+    save: [string]
+        name of the figure
+    dire: ['x', 'y', 'z']
+        direction of the detector
+    '''
+    file = pd.read_csv(file)
 
-file = 'p1/input_line_0001.csv'
-legend = 'p1'
-plotcsv_frommoose_temp(file, legend, dire='x')
+    if dire == 'r':
+        x = np.array(file['x'].tolist())
+        y = np.array(file['y'].tolist())
+        d = np.sqrt(x**2 + y**2)
+    else:
+        d = file[dire].tolist()
 
-file = 'p3/input_line_0001.csv'
-legend = 'p3'
-file = pd.read_csv(file)
-flux0 = np.array(file['flux0'].tolist())
-flux2 = np.array(file['flux2'].tolist())
-flux = flux0 - 2*flux2
-x = np.array(file['x'].tolist())
-plt.plot(x, flux, label=legend)
+    for g in range(G):
+        if diff is True:
+            flux = np.array(file['flux'+str(g+1)].tolist())
+            legend = 'Diff'
+        else:
+            flux0 = np.array(file['flux0_'+str(g+1)].tolist())
+            flux2 = np.array(file['flux2_'+str(g+1)].tolist())
+            flux = flux0 - 2*flux2
+            legend = 'P3'
 
-plt.legend(loc='upper right')
-plt.savefig(save, dpi=300, bbox_inches="tight")
+        if fix is False and g == 0:
+            M = max(flux)
+        flux /= M
+        plt.plot(d, flux, label=legend+', g='+str(g+1))
+
+    plt.legend(loc='upper right')
+    plt.xlabel(dire + ' [cm]')
+    plt.ylabel(r'$\phi \left[\frac{n}{cm^2s}\right]$')
+
+
+# save = 'output-1g-fixed'
+# plt.figure()
+# file = 'p3/input-1g-fixed_line_0001.csv'
+# plotcsv_frommoose_1G(file, save, diff=False, fix=True, dire='x')
+# file = 'diffusion/input-1g-fixed_line_0001.csv'
+# plotcsv_frommoose_1G(file, save, diff=True, fix=True, dire='x')
+# plt.savefig(save, dpi=300, bbox_inches="tight")
+
+# save = 'output-1g-crit'
+# plt.figure()
+# file = 'p3/input-1g-crit_line_0001.csv'
+# plotcsv_frommoose_1G(file, save, diff=False, fix=False, dire='x')
+# file = 'diffusion/input-1g-crit_line_0001.csv'
+# plotcsv_frommoose_1G(file, save, diff=True, fix=False, dire='x')
+# plt.savefig(save, dpi=300, bbox_inches="tight")
+
+# save = 'output-3g-fixed'
+# plt.figure()
+# file = 'p3/input-3g-fixed_line_0001.csv'
+# plotcsv_frommoose_multi(file, save, diff=False, fix=True, dire='x')
+# file = 'diffusion/input-3g-fixed_line_0001.csv'
+# plotcsv_frommoose_multi(file, save, diff=True, fix=True, dire='x')
+# plt.savefig(save, dpi=300, bbox_inches="tight")
+
+# save = 'output-3g-crit'
+# plt.figure()
+# file = 'p3/input-3g-crit_line_0001.csv'
+# plotcsv_frommoose_multi(file, save, diff=False, fix=False, dire='x')
+# file = 'diffusion/input-3g-crit_line_0001.csv'
+# plotcsv_frommoose_multi(file, save, diff=True, fix=False, dire='x')
+# plt.savefig(save, dpi=300, bbox_inches="tight")
