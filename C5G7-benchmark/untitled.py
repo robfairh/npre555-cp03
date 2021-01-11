7d29707acb3ebace498d8517dbc3ef60e63fc311
robfairh <ref3@illinois.edu>
2021-01-11 09:37:25 -0300

    completed geometry

---
+++ C5G7-benchmark/untitled.py
@@ -0,0 +1,22 @@
+# iin = 2322
+# for i in range(6):
+# 	print(iin, ', ',  iin+6, ', ',  iin+12, ', ',  iin+18, ', ',  iin+24, ', ',  iin+30)
+# 	iin += 4
+# 	print(iin, ', ',  iin+6, ', ',  iin+12, ', ',  iin+18, ', ',  iin+24, ', ',  iin+30)
+# 	iin += 32
+
+# 8158
+# 8161
+
+iinb = 2520
+for i in range(33):
+	print(iinb, ', ',  iinb+4, ', ')
+
+	iin = iinb + 6
+	for i in range(6):
+		print(iin, ', ',  iin+5, ', ',  iin+10, ', ',  iin+15, ', ',  iin+20, ', ',  iin+25, ', ')
+		iin += 3
+		print(iin, ', ',  iin+5, ', ',  iin+10, ', ',  iin+15, ', ',  iin+20, ', ',  iin+25, ', ')
+		iin += 27
+
+	iinb += 171
