43816b86a78dec47c1e0cd66ea5f90f3719be793
robfairh <ref3@illinois.edu>
2021-01-27 12:11:13 -0300

    working on constants

--- C5G2-original/constants.py
+++ C5G2-original/constants.py
@@ -553,20 +553,263 @@ def create_xs_diag(outdir, temp, materials):
                 fh.write('\n')
 
 
+def update_dict(dictio, key, value):
+    ''' updates dictionary by adding a key with an associated value
+    '''
+    dictio[key] = value
+    return dictio
+
+
+def uo2_properties():
+    # UO2
+    mat = {}
+    value = np.array([1.20, 0.40])
+    mat = update_dict(mat, 'diff', value)
+    value = np.array([0.010, 0.100])
+    mat = update_dict(mat, 'absxs', value)
+    value = np.array([0.02, 0.])
+    mat = update_dict(mat, '', value)
+    value = np.array([])
+    mat = update_dict(mat, '', value)
+    value = np.array([])
+    mat = update_dict(mat, '', value)
+    value = np.array([])
+    mat = update_dict(mat, '', value)
+    value = np.array([])
+    mat = update_dict(mat, '', value)
+    value = np.array([[], []])
+    mat = update_dict(mat, 'scxs', value)
+
+    data = {
+        'uo2': {
+                'D': np.array([]),
+                'SA': np.array([]),
+                'SR': np.array([0.02, 0.]),
+                'NSF': np.array([0.005, 0.125]),
+                'SS': np.array([0.54, 1.00]),
+               },
+        'mox3': {
+                'D': np.array([1.20, 0.40]),
+                'SA': np.array([0.015, 0.200]),
+                'SR': np.array([0.015, 0.]),
+                'NSF': np.array([0.0075, 0.300]),
+                'SS': np.array([0.52, 0.90]),
+               },
+        'mox2': {
+                'D': np.array([1.20, 0.40]),
+                'SA': np.array([0.015, 0.250]),
+                'SR': np.array([0.015, 0.]),
+                'NSF': np.array([0.0075, 0.375]),
+                'SS': np.array([0.52, 0.83]),
+               },
+        'mox1': {
+                'D': np.array([1.20, 0.40]),
+                'SA': np.array([0.015, 0.300]),
+                'SR': np.array([0.015, 0.]),
+                'NSF': np.array([0.0075, 0.45]),
+                'SS': np.array([0.52, 0.76]),
+               },
+        'gtub': {
+                'D': np.array([1.20, 0.40]),
+                'SA': np.array([0.001, 0.02]),
+                'SR': np.array([0.025, 0.]),
+                'NSF': np.array([0., 0.]),
+                'SS': np.array([0.56, 1.20]),
+               },
+        'reflec': {
+                'D': np.array([1.20, 0.20]),
+                'SA': np.array([0.001, 0.04]),
+                'SR': np.array([0.05, 0.]),
+                'NSF': np.array([0., 0.]),
+                'SS': np.array([0.56, 2.30]),
+               },
+        'fchamb': {
+                'D': np.array([1.20, 0.40]),
+                'SA': np.array([0.001, 0.02]),
+                'SR': np.array([0.025, 0.]),
+                'NSF': np.array([1e-7, 3e-6]),
+                'SS': np.array([0.56, 1.20]),
+               }
+    }
+
+    mat2 = prepare_xs(mat)
+    return mat2
+
+
+def materials_het_moltres():
+    '''
+    Cross-sections from Cavarec et al. Benchmark Calculations
+    of Power Distributions within Assemblies. 1994.
+    Applies the diagonal transport correction.
+
+    # diagonal transport correction (for each group):
+    # str = sa + (1-mu)*ss
+    # ss* = ss + (STR-STOT)
+
+    Returns:
+    --------
+    materials: [dictionary]
+        dictionary that contains the materials and their respective
+        cross-sections.
+        * main keys:
+            - uo2 - U - UO2 Fuel
+            - mox3 - P1 - 4.3% MOX Fuel
+            - mox2 - P2 - 7.0% MOX Fuel
+            - mox1 - P3 - 8.7% MOX Fuel
+            - gtub - X - Guide Tube
+            - reflec - R - Reflector
+            - fchamb - C - Moveable Fission Chamber
+
+        * secondary keys:
+            - D = difffusion coefficients
+            - SA = absorption cross-sections
+            - SR = removal scattering = [s12, s21]
+            - NSF = production cross-sections
+            - SS = self-scattering = [s11, s22]
+    '''
+
+    data = {
+        'uo2': {
+                'D': np.array([1.20, 0.40]),
+                'SA': np.array([0.010, 0.100]),
+                'SR': np.array([0.02, 0.]),
+                'NSF': np.array([0.005, 0.125]),
+                'SS': np.array([0.54, 1.00]),
+               },
+        'mox3': {
+                'D': np.array([1.20, 0.40]),
+                'SA': np.array([0.015, 0.200]),
+                'SR': np.array([0.015, 0.]),
+                'NSF': np.array([0.0075, 0.300]),
+                'SS': np.array([0.52, 0.90]),
+               },
+        'mox2': {
+                'D': np.array([1.20, 0.40]),
+                'SA': np.array([0.015, 0.250]),
+                'SR': np.array([0.015, 0.]),
+                'NSF': np.array([0.0075, 0.375]),
+                'SS': np.array([0.52, 0.83]),
+               },
+        'mox1': {
+                'D': np.array([1.20, 0.40]),
+                'SA': np.array([0.015, 0.300]),
+                'SR': np.array([0.015, 0.]),
+                'NSF': np.array([0.0075, 0.45]),
+                'SS': np.array([0.52, 0.76]),
+               },
+        'gtub': {
+                'D': np.array([1.20, 0.40]),
+                'SA': np.array([0.001, 0.02]),
+                'SR': np.array([0.025, 0.]),
+                'NSF': np.array([0., 0.]),
+                'SS': np.array([0.56, 1.20]),
+               },
+        'reflec': {
+                'D': np.array([1.20, 0.20]),
+                'SA': np.array([0.001, 0.04]),
+                'SR': np.array([0.05, 0.]),
+                'NSF': np.array([0., 0.]),
+                'SS': np.array([0.56, 2.30]),
+               },
+        'fchamb': {
+                'D': np.array([1.20, 0.40]),
+                'SA': np.array([0.001, 0.02]),
+                'SR': np.array([0.025, 0.]),
+                'NSF': np.array([1e-7, 3e-6]),
+                'SS': np.array([0.56, 1.20]),
+               }
+    }
+
+    materials = {}
+    for mat in data:
+        materials[mat] = {}
+
+        # tot xs
+        sa = data[mat]['SA']
+        ss = data[mat]['SS']
+        sr = data[mat]['SR']
+        stot = sa + ss + sr
+        materials[mat]['totxs'] = stot
+
+        # nf xs
+        materials[mat]['nfxs'] = data[mat]['NSF']
+
+        # transport correction
+        sa = data[mat]['SA']
+        ss = data[mat]['SS']
+        mu = np.array([0.5, 0.3])
+        
+        # materials[mat]['trxs'] = sa + (1-mu)*ss
+        materials[mat]['trxs'] = 1./3./data[mat]['D']
+
+        # scatt xs: SP0
+        ss = data[mat]['SS']
+        sr = data[mat]['SR']
+
+        materials[mat]['scxs'] = np.zeros(4)
+        materials[mat]['scxs'][0] = ss[0]
+        materials[mat]['scxs'][1] = sr[0]
+        materials[mat]['scxs'][2] = sr[1]
+        materials[mat]['scxs'][3] = ss[1]
+
+    # str = sa + (1-mu)*ss
+    # ss* = ss + (STR-STOT)
+
+    return materials
+
+
+
+def output_xs(outdir, temp, materials):
+    '''
+    This function outputs the dictionary with the material cross-sections
+    into the SP3 App readable format.
+
+    '''
+    for currentMat in materials.keys():
+        for data in materials[currentMat].keys():
+
+            with open(outdir + '/' + currentMat +
+                      '_' + data + '.txt', 'a') as fh:
+
+                strData = materials[currentMat][data]
+                strData = ' '.join(
+                    [str(dat) for dat in strData]) if isinstance(
+                    strData, np.ndarray) else strData
+                fh.write(str(temp) + ' ' + strData)
+                fh.write('\n')
+
+
 if __name__ == "__main__":
     temp = 300
 
-    # produces cross-sections for the heterogeneous case
-    outdir = 'xs2g'
-    if os.path.exists(outdir):
-        shutil.rmtree(outdir)
-    os.mkdir(outdir)
+    # # produces cross-sections for the heterogeneous case
+    # outdir = 'xs2g'
+    # if os.path.exists(outdir):
+    #     shutil.rmtree(outdir)
+    # os.mkdir(outdir)
+
     # materials = materials_het()
     # create_xs(outdir, temp, materials)
-    materials = materials_het_diag()
-    create_xs_diag(outdir, temp, materials)
+    # materials = materials_het_diag()
+    # create_xs_diag(outdir, temp, materials)
 
     # # produces cross-sections for the homogeneous case
     # outdir = 'xs2g-homo'
     # materials = materials_hom()
     # create_xs(outdir, temp, materials)
+
+    outdir = 'xs2g-m'
+    if os.path.exists(outdir):
+        shutil.rmtree(outdir)
+    os.mkdir(outdir)
+
+    materials = {}
+    materials['uo2'] = uo2_properties()
+    materials['mox1'] = mox1_properties()
+    materials['mox2'] = mox2_properties()
+    materials['mox3'] = mox3_properties()
+    materials['fisc'] = fission_properties()
+    materials['gtube'] = guide_properties()
+    materials['moder'] = moderator_properties()
+    materials['reflec'] = moderator_properties()
+    output_xs(outdir, temp, materials)
